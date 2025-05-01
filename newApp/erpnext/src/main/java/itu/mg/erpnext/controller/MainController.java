12e03e73d1fad4a9730fabab6f5717adcd0cd820
tahinaBemax <tahina.nambinintsoa@passerellesnumeriques.org>
2025-05-01 12:50:12 +0300

    login terminé

---
+++ newApp/erpnext/src/main/java/itu/mg/erpnext/controller/MainController.java
@@ -0,0 +1,6 @@
+package itu.mg.erpnext.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+}
